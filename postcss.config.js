3e62fe8a5e6bab37b897e6b4c33fb560b0e5294c
kvachte <karel.vanachte@ugent.be>
2024-06-26 16:03:47 +0200

    Added :host to purgecss safelist

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,7 @@ export default {
     purgecss({
       content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx,html}"],
       safelist: {
-        //standard: [":host"], // Safelist patterns
+        standard: [":host"], // Safelist patterns
       },
       defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
     }),
